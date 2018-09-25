83c1634265ae610391ee96af75bd4afdbd74bf1d
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-09-25 20:13:31 +0200

    strtoupper the whole ids string

--- res/scripts/functions.php
+++ res/scripts/functions.php
@@ -1764,7 +1764,7 @@
 	}
 	
 	function getUnitsToCompare($get, $dataUnits){
-		$list = array_unique(array_map('strtoupper', explode(',', $get)));
+		$list = array_unique(explode(',', strtoupper($get)));
 		$toCompare = array();
 		
 		for ($i = 0; $i < sizeOf($dataUnits); $i++){
